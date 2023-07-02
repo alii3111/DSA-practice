98985cbf7c4a83cec3c2ce1ff267c10568e3f9d0
alii3111 <aliimohamud185@gmail.com>
2023-07-02 08:55:36 +0300

    add buble_sort function

--- Bubble_Sort/bubble_sort.py
+++ Bubble_Sort/bubble_sort.py
@@ -1,3 +1,17 @@
 # list = [12,8,3,1,6,4]
 # sort this list in an ascending order using bubble sort
 
+def bubble_sort(list):
+    size = len(list) - 1
+    sorted = False
+
+    while not sorted:
+        sorted = True
+        for i in range(size):
+            if list[i] > list[i + 1]:
+                sorted = False
+                list[i], list[i+1] = list[i+1], list[i]
+    print(list)
+list = [12,8,3,1,6,4]
+
+bubble_sort(list)
