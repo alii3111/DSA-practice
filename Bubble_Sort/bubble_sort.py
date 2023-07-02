484d1d68727dbdedfd200c9f029135582bc7309d
alii3111 <aliimohamud185@gmail.com>
2023-07-02 09:01:31 +0300

    add steps of the operation in buble_sort function

--- Bubble_Sort/bubble_sort.py
+++ Bubble_Sort/bubble_sort.py
@@ -5,13 +5,16 @@ def bubble_sort(list):
     size = len(list) - 1
     sorted = False
 
+    steps = 0
     while not sorted:
         sorted = True
         for i in range(size):
+            steps+=1
             if list[i] > list[i + 1]:
                 sorted = False
                 list[i], list[i+1] = list[i+1], list[i]
     print(list)
+    print(steps)
 list = [12,8,3,1,6,4]
 
 bubble_sort(list)
