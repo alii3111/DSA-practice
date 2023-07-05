0b4677f53aa2155c8e19a3ba98a052f562f4dbb8
alii3111 <aliimohamud185@gmail.com>
2023-07-05 14:20:33 +0300

    add insertion_fn in inserion_sort file

--- Insertion_Sort/insertion_sort.py
+++ Insertion_Sort/insertion_sort.py
@@ -1,2 +1,6 @@
 # using insertion sort, sort the following list in asscending order
 # list_a = [4,2,7,9,1,3]
+
+def insertion_sort(list):
+    for i in range(1, len(list)):
+        values_to_sort = list[i]
